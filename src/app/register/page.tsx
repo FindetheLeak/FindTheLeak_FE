2fbca8646f9a276931f051c8abb4681dc4ae8537
dasosann <goodtony0403@gmail.com>
2025-12-03 03:05:53 +0900

    feat: Suspense 추가

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ScreenRegister from "@/features/register/components/ScreenRegister";
 
 export default function RegisterPage() {
-  return <ScreenRegister />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ScreenRegister />
+    </Suspense>
+  );
 }
